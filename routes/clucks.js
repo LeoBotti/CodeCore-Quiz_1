ae26437fe6c8bfbe18d5b4c0e84460aafb003522
Nicholas Chan <nicholaschan1994@hotmail.com>
2018-08-27 17:12:30 -0700

    Implement Sign In. Create clucks_db with clucks table. Implement Create Clucks. Implement Clucks Index.

---
+++ routes/clucks.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const knex = require("../db/client");
+
+router.get("/new", (req, res) => {
+  res.render("clucks/new");
+})
+
+router.post("/new", (req, res) => {
+  const { content, imageURL, createdAt } = req.body
+  const username = req.cookies.username;
+
+  knex("clucks")
+    .insert({
+      content,
+      imageURL,
+      createdAt,
+      username
+    })
+    .then(() => {
+      res.redirect("/clucks");
+    })
+})
+
+router.get("/", (req, res) => {
+  knex("clucks")
+    .orderBy("createdAt", "desc")
+    .then(clucks => {
+      res.render("clucks/index", { clucks });
+    });
+})
+
+module.exports = router;
